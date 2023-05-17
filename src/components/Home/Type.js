a0400989d1203fa54e015eeb82d525df963144fe
Petros Devrikis <petrosdevri@protonmail.com>
2023-05-17 16:01:48 +0300

    Fixed Type order.

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -3,9 +3,9 @@ import Typewriter from 'typewriter-effect';
 export default function Type() {
     return (
         <Typewriter options={{
-            strings: ['Aspiring Data Analyst',
+            strings: ['Data Scientist',
+                'Data Analyst',
                 'Business Intelligence Specialist',
-                'Data Scientist',
                 'Data Engineer',],
             autoStart: true,
             loop: true,
