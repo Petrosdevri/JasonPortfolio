48003dbe180f023797227ed54bcd71590a0fafbc
Petros Devrikis <100411628+Petrosdevri@users.noreply.github.com>
2023-06-29 22:57:59 +0300

    Add SpaceX project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -22,9 +22,10 @@ export default function Projects() {
             <Stack className='projects-stack' direction='horizontal' gap={5}>
                 <ProjectCard title='Credit Card Fraud Detection' description='An ML model built with Keras, NumPy, Pandas and Seaborn to detect credit card fraud cases.' picture={CreditCardFraudDetection} code='https://github.com/JasonKeha/Machine_Learning-ML-/blob/main/Credit_Card_Fraud_Detection_(ML).ipynb' />
                 <ProjectCard title='Forbes Sport Earnings' description='Tracks sport earnings based on a Forbes dataset using aforementioned libraries and prints out results.' picture={ForbesSportEarnings} code='https://github.com/JasonKeha/Exploratory_Data_Analysis-EDA-/blob/main/Forbes%20Sport%20Earnings%20(EDA).ipynb' />
-                <ProjectCard title='Students Exams' description='Tracks student performance based on a dataset using aforementioned libraries and prints out results.' picture={StudentsExams} code='https://github.com/JasonKeha/Exploratory_Data_Analysis-EDA-/blob/main/Students%20Exams%20(EDA).ipynb' />
+                <ProjectCard title='SpaceX Megaproject' description='A megaproject combining data analysis to investigate into SpaceX data.' code='https://github.com/JasonKeha/Space_X_Megaproject' />
             </Stack>
             <Stack className='projects-stack' direction='horizontal' gap={5}>
+                <ProjectCard title='Students Exams' description='Tracks student performance based on a dataset using aforementioned libraries and prints out results.' picture={StudentsExams} code='https://github.com/JasonKeha/Exploratory_Data_Analysis-EDA-/blob/main/Students%20Exams%20(EDA).ipynb' />
                 <ProjectCard title='SQL Murder Mystery' description='A thrilling mystery about a murder in SQL City uncovered in MySQL and Pandas.' picture={SQLMurderMystery} code='https://github.com/JasonKeha/Structured_Query_Language-SQL-/blob/main/The_SQL_Murder_Mystery.ipynb' />
                 <ProjectCard title='SQL Summer Olympic Medals' description='A MySQL & Pandas project which displays medals won by every country in every Summer Olympics competition.' picture={SQLSummerOlympics} code='https://github.com/JasonKeha/Structured_Query_Language-SQL-/blob/main/SQL_Summer_Olympic_Medals.ipynb' />
             </Stack>
